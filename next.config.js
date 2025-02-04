1f11a3793c0cf5ac7519594b750c4432ef955cfe
kishan kumawat <kishankumawat1271@gamil.com>
2025-02-04 19:13:11 +0530

    change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,52 @@
-const path = require('path')
+// const path = require('path')
+
+
+
+
  
-module.exports = {
+// module.exports = {
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//         pathname: '**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'media.dev.to',
+//         pathname: '**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'media2.dev.to',
+//         pathname: '**',
+//       },
+//     ],
+//   },
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/protofolio-kishan',  // GitHub Pages repo name
+  assetPrefix: '/protofolio-kishan/', // GitHub Pages repo name
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
@@ -23,4 +69,6 @@ module.exports = {
       },
     ],
   },
-}
+};
+
+module.exports = nextConfig;
