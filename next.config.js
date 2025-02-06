c69bf0447d5eab8f521a1c949ba726a3be614c4f
kishan kumawat <kishankumawat1271@gamil.com>
2025-02-06 23:12:44 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,52 +1,10 @@
-// const path = require('path')
+const path = require('path')
 
 
 
 
  
-// module.exports = {
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, 'styles')],
-//   },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'res.cloudinary.com',
-//         pathname: '**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'media.dev.to',
-//         pathname: '**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'media2.dev.to',
-//         pathname: '**',
-//       },
-//     ],
-//   },
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-const path = require('path');
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/protofolio-kishan',  // GitHub Pages repo name
-  assetPrefix: '/protofolio-kishan/', // GitHub Pages repo name
+module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
@@ -69,6 +27,44 @@ const nextConfig = {
       },
     ],
   },
-};
+}
 
-module.exports = nextConfig;
+
+
+
+
+
+
+
+// const path = require('path');
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   basePath: '/protofolio-kishan',  // GitHub Pages repo name
+//   assetPrefix: '/protofolio-kishan/', // GitHub Pages repo name
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//         pathname: '**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'media.dev.to',
+//         pathname: '**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'media2.dev.to',
+//         pathname: '**',
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
