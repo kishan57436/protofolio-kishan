c905d19782d28a598fe71ddd6b66116eadfed3b3
kishan kumawat <kishankumawat1271@gamil.com>
2025-02-16 00:40:48 +0530

    changes lottie

--- app/component/helper/animation-lottie.jsx
+++ app/component/helper/animation-lottie.jsx
@@ -1,20 +1,43 @@
-"use client"
+// "use client"
 
+// import Lottie from "lottie-react";
+
+// const AnimationLottie = ({ animationPath, width }) => {
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationPath,
+//     style: {
+//       width: '95%',
+//     }
+//   };
+
+//   return (
+//     <Lottie {...defaultOptions} />
+//   );
+// };
+
+// export default AnimationLottie;
+
+
+"use client";
+
+import { useEffect, useState } from "react";
 import Lottie from "lottie-react";
 
-const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+const AnimationLottie = ({ animationPath }) => {
+  const [animationData, setAnimationData] = useState(null);
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
+  useEffect(() => {
+    fetch(animationPath)
+      .then((response) => response.json())
+      .then((data) => setAnimationData(data))
+      .catch((error) => console.error("Error loading animation:", error));
+  }, [animationPath]);
+
+  if (!animationData) return <p>Loading...</p>;
+
+  return <Lottie animationData={animationData} loop autoplay style={{ width: "95%" }} />;
 };
 
 export default AnimationLottie;
