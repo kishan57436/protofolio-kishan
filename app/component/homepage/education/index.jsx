d5df98ed26403bdbdb5627571b46aaaffd06c22d
kishan kumawat <kishankumawat1271@gamil.com>
2025-02-16 00:52:57 +0530

    lotte remove

--- app/component/homepage/education/index.jsx
+++ app/component/homepage/education/index.jsx
@@ -2,8 +2,8 @@
 import { educations } from "@/utils/data/educations";
 import Image from "next/image";
 // import { BsPersonWorkspace } from "react-icons/bs";
-import lottieFile from '../../../assets/lottie/study.json';
-import AnimationLottie from "../../helper/animation-lottie";
+// import lottieFile from '../../../assets/lottie/study.json';
+// import AnimationLottie from "../../helper/animation-lottie";
 // import GlowCard from "../../helper/glow-card";
 
 function Education() {
@@ -36,7 +36,7 @@ function Education() {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
           <div className="flex justify-center items-start">
             <div className="w-3/4 h-3/4">
-              <AnimationLottie animationPath={lottieFile} />
+              {/* <AnimationLottie animationPath={lottieFile} /> */}
             </div>
           </div>
 
