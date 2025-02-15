6b0bc599a58d07497e4e9dddcda85ea7533d7946
kishan kumawat <kishankumawat1271@gamil.com>
2025-02-16 01:13:35 +0530

    lottie file added

--- app/component/helper/animation-lottie.jsx
+++ app/component/helper/animation-lottie.jsx
@@ -1,43 +1,43 @@
-// "use client"
+"use client"
 
-// import Lottie from "lottie-react";
-
-// const AnimationLottie = ({ animationPath, width }) => {
-//   const defaultOptions = {
-//     loop: true,
-//     autoplay: true,
-//     animationData: animationPath,
-//     style: {
-//       width: '95%',
-//     }
-//   };
-
-//   return (
-//     <Lottie {...defaultOptions} />
-//   );
-// };
-
-// export default AnimationLottie;
-
-
-"use client";
-
-import { useEffect, useState } from "react";
 import Lottie from "lottie-react";
 
-const AnimationLottie = ({ animationPath }) => {
-  const [animationData, setAnimationData] = useState(null);
+const AnimationLottie = ({ animationPath, width }) => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationPath,
+    style: {
+      width: '95%',
+    }
+  };
 
-  useEffect(() => {
-    fetch(animationPath)
-      .then((response) => response.json())
-      .then((data) => setAnimationData(data))
-      .catch((error) => console.error("Error loading animation:", error));
-  }, [animationPath]);
-
-  if (!animationData) return <p>Loading...</p>;
-
-  return <Lottie animationData={animationData} loop autoplay style={{ width: "95%" }} />;
+  return (
+    <Lottie {...defaultOptions} />
+  );
 };
 
 export default AnimationLottie;
+
+
+// "use client";
+
+// import { useEffect, useState } from "react";
+// import Lottie from "lottie-react";
+
+// const AnimationLottie = ({ animationPath }) => {
+//   const [animationData, setAnimationData] = useState(null);
+
+//   useEffect(() => {
+//     fetch(animationPath)
+//       .then((response) => response.json())
+//       .then((data) => setAnimationData(data))
+//       .catch((error) => console.error("Error loading animation:", error));
+//   }, [animationPath]);
+
+//   if (!animationData) return <p>Loading...</p>;
+
+//   return <Lottie animationData={animationData} loop autoplay style={{ width: "95%" }} />;
+// };
+
+// export default AnimationLottie;
